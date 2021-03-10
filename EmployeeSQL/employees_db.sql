db1580eb746736c9a8c04313e2de35257dd09380
Gerald Watts <geraldwattsthethird@gmail.com>
2021-03-09 20:44:59 -0600

    class update

--- EmployeeSQL/employees_db.sql
+++ EmployeeSQL/employees_db.sql
@@ -8,15 +8,15 @@ drop table titles;
 create table department_employees (
 	emp_no INT not null,
 	dept_no varchar(30) not null,
-	primary key (emp_no),
-	COPY department_employees
+	foreign key (emp_no) references employees(emp_no),
+	foreign Key (dept_no) references departments(dept_no)
 );
 
 select * from department_employees;
 
 create table department_manager (
 	dept_no varchar(30) not null,
-	emp_no varchar(30) not null,
+	emp_no int not null,
 	primary key (emp_no)
 );
 
@@ -66,9 +66,24 @@ inner join salaries on
 employees.emp_no = salaries.emp_no;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
-select emp_no, first_name, last_name, hire_date from Employees
+select emp_no, first_name, last_name, hire_date from employees
 where hire_date >= '1985-12-31'
 and hire_date < '1987-01-01';
 
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select department_manager.dept_no, departments.dept_name, department_manager.emp_no, employees.last_name, employees.first_name
+from department_manager
+inner join departments ON
+department_manager.dept_no = departments.dept_no
+inner join employees ON
+department_manager.emp_no = employees.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+inner join department_manager ON
+employees.emp_no = department_manager.emp_no
+inner join departments ON
+department_manager.dept_no = departments.dept_no;
 
 
